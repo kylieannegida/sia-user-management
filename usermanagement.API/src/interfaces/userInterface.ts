1114e5b0f9183aa944fc6fac2c1c441785042a78
Kylie Anne Gida <kylieannegida@gmail.com>
2024-12-15 12:56:31 +0800

    Delete usermanagement.API directory

--- usermanagement.API/src/interfaces/userInterface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IUser extends Document {
-  _id: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-}
